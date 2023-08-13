48cda097148d710c0acbdbcdf87410c72ca5f410
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-08-14 02:36:47 +0530

    :fire: completed the navbar for the big screen

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
-
+import './navbar.css';
+import Logo from '../../assets/logo-agp.jpg'
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className='navbar'>
+      <div className="left">
+        <img src={Logo} alt="" />
+        <h3>Astro Gems Product</h3>
+      </div>
+      <div className="right">
+      <ul>
+      <li className='active-listItem'>Home</li>
+      <li className='normal-listItem'>Services</li>
+      <li className='normal-listItem'>Products</li>
+      <li className='normal-listItem'>About</li>
+      <li className='normal-listItem'>Contact</li>
+      </ul>
+      </div>
+     
     </div>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import "./home.css";
+import Navbar from "../../components/navbar/Navbar";
 const Home = () => {
   return (
-    <div>
-      
+    <div className='home'>
+      <div className="hero">
+      <Navbar />
+      <div className="hero-section">
+        hi i am hero section.
+      </div>
+      </div>
     </div>
   )
 }
