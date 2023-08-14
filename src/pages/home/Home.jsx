8e9d1936e86b3e85827b4d6131065eff24f52e74
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-08-15 01:38:23 +0530

    :fire: created the hero section of the website not responsive

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,18 +1,31 @@
-import React from 'react'
+import React from "react";
 import "./home.css";
 import Navbar from "../../components/navbar/Navbar";
+import AstroSarthi from "../../assets/astroSarthi.png";
 const Home = () => {
   return (
-    <div className='home'>
+    <div className="home">
       <div className="hero">
-      <Navbar />
-      <div className="hero-section">
-        hi i am hero section.
+        <Navbar />
+        <div className="hero-section">
+          <div className="left-hero">
+            <h1>Unveil Destiny's Secrets with Gem Astrology</h1>
+            <p>Explore the mystical world of astrology and gems with AstroGems. Discover celestial insights and adorn yourself with exquisite gem treasures for a cosmic journey like no other.</p>
+            <div className="btn-cont">
+              <button>GET STARTED</button>
+              <button>CONTACT US</button>
+            </div>
+          </div>
+          <div className="right-hero">
+            <img src={AstroSarthi} alt="" />
+          </div>
+        </div>
       </div>
+      <div className="rings">
+        
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
-
+export default Home;
