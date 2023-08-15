39e02558ba95090ec1a9ba937e35db45ef9cb05c
JatinBisht2308 <jatinbisht2308@gmail.com>
2023-08-16 03:35:41 +0530

    :fire: added the marquee for the zodiac sign

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 import "./home.css";
+import Marquee from "react-fast-marquee";
 import Navbar from "../../components/navbar/Navbar";
 import AstroSarthi from "../../assets/astroSarthi.png";
+import Z1 from "../../assets/zodiacs/ai1.png";
+import Z2 from  "../../assets/zodiacs/ai2.png";
+import Z3 from  "../../assets/zodiacs/ai3.png";
+import Z4 from  "../../assets/zodiacs/ai4.png";
+import Z5 from  "../../assets/zodiacs/ai5.png";
+import Z6 from  "../../assets/zodiacs/ai6.png";
+import Z7 from  "../../assets/zodiacs/ai7.png";
+import Z8 from  "../../assets/zodiacs/ai8.png";
+import Z9 from  "../../assets/zodiacs/ai9.png";
+import Z10 from  "../../assets/zodiacs/ai10.png";
+import Z11 from  "../../assets/zodiacs/ai11.png";
+import Z12 from  "../../assets/zodiacs/ai12.png";
 const Home = () => {
   return (
     <div className="home">
@@ -21,10 +34,21 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div className="rings">
-        
+     <Marquee loop={0} speed={100} gradientColor={false} className="marq">
+      <img src={Z1} alt="" />
+      <img src={Z2} alt="" />
+      <img src={Z3} alt="" />
+      <img src={Z4} alt="" />
+      <img src={Z5} alt="" />
+      <img src={Z6} alt="" />
+      <img src={Z7} alt="" />
+      <img src={Z8} alt="" />
+      <img src={Z9} alt="" />
+      <img src={Z10} alt="" />
+      <img src={Z11} alt="" />
+      <img src={Z12} alt="" />
+     </Marquee>
       </div>
-    </div>
   );
 };
 
